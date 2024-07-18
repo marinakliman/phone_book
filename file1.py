e551ce5e5c5a7df8e1b99d1ade00b7627f574afc
Marina Kliman <marinakliman9@gmail.com>
2024-07-18 21:15:51 +1000

    Загрузка телефонногоправочника

---
+++ file1.py
@@ -0,0 +1,16 @@
+def load_phonebook(filename):
+    phonebook = []
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            for line in file:
+                parts = line.strip().split(',')
+                if len(parts) == 4:
+                    phonebook.append({
+                        'last_name': parts[0],
+                        'first_name': parts[1],
+                        'middle_name': parts[2],
+                        'phone_number': parts[3]
+                    })
+    except FileNotFoundError:
+        print(f"Файл {filename} не найден, создаем новый справочник.")
+    return phonebook
